1f6508023c321b5bbef801d5178c1f19546883e0
Georgi Donchev <georgeuk26@gmail.com>
2025-03-14 17:57:01 +0000

    Extract some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+
 function App() {
 
 	return (
 		<div id="box">
+			<Header />
 
+			<main id="main-content">
+				<Home />
+			</main>
 		</div>
 	)
 }
