a0b25c2c4b99e832e9b8de04e82777a867a3340f
Georgi Donchev <georgeuk26@gmail.com>
2025-03-15 17:00:53 +0000

    Empty games stream

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -15,9 +15,12 @@ export default function GameCatalog() {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(game => <GameCatalogItem key={game._id} {...game} />)}            
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
+
+            {games.length > 0 
+                ? games.map(game => <GameCatalogItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }         
+           
         </section>
     );
 }
