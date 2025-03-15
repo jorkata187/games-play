e5b3a1b2c3f3785328686ed3ae65ef63e1af422a
Georgi Donchev <georgeuk26@gmail.com>
2025-03-15 19:15:53 +0000

    Extract comments related jsx in separate components

---
+++ client/src/components/coments-create/ComentsCreate.jsx
@@ -0,0 +1,11 @@
+export default function ComentsCreate() {
+   return (
+            <article className="create-comment">
+                <label>Add new comment:</label>
+                <form className="form">
+                    <textarea name="comment" placeholder="Comment......"></textarea>
+                    <input className="btn submit" type="submit" value="Add Comment" />
+                </form>
+            </article>
+    );
+}
